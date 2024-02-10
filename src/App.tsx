a7f4fe21e8aeff55eec860deabcf738389bcb699
briansleonel <gonzalezbrianleonel76@gmail.com>
2024-02-10 12:04:04 -0300

    feat: add phrases

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useRef, useState } from "react";
+import phrases from "./phrases";
 
 function App() {
     const [noCount, setNoCount] = useState(0);
@@ -12,6 +13,10 @@ function App() {
 
     const [x, setX] = useState(0);
     const [y, setY] = useState(0);
+
+    function getNoButtonText() {
+        return phrases[Math.min(noCount, phrases.length - 1)];
+    }
 
     function handleHoverNo() {
         setFirstNo(false);
@@ -38,7 +43,7 @@ function App() {
 
     return (
         <div
-            className="w-full max-h-screen h-screen flex flex-col justify-center items-center gap-16 relative p-4 md:p-12 transition-all"
+            className="w-full max-h-screen h-screen bg-zinc-950 text-zinc-50 flex flex-col justify-center items-center gap-16 relative p-4 md:p-12 transition-all"
             ref={refContainer}
         >
             {yesPressed ? (
@@ -56,7 +61,7 @@ function App() {
                         //ref={refContainer}
                     >
                         <div className="text-4xl md:text-7xl uppercase font-normal md:font-light text-center">
-                            ¿Quieres ser mi novia?
+                            ¿Querés ser mi 14 de febrero?
                         </div>
                         <div className="grid grid-cols-2 gap-8 md:gap-16 text-white">
                             <button
@@ -79,6 +84,12 @@ function App() {
                             </button>
                         </div>
                     </div>
+
+                    {!firstNo && (
+                        <div className="w-full md:w-auto p-6 px-8 md:p-8 md:px-14 mt-10 bg-zinc-900 rounded text-xl md:text-4xl font-light text-center">
+                            {getNoButtonText()}
+                        </div>
+                    )}
                 </>
             )}
         </div>
---
+++ src/phrases.ts
@@ -0,0 +1,19 @@
+const phrases = [
+    "¿Segura?",
+    "¿De verdad segura?",
+    "¡Por favor! :c",
+    "Por favor pensalo...",
+    "No me hagas esto pipipi",
+    "Porfis decime que sí",
+    "Si decís que no, me voy a poner muy triste",
+    "Dale, decí que sí",
+    "Estaré muy triste...",
+    "Estaré muy muy triste :c",
+    "Estaré muy muy muy muy triste T_T",
+    "Bueno, dejaré de preguntar...",
+    "Solo bromeaba, POR FAVOR DÍ QUE SI",
+    "Voy a llorar",
+    "Rompes el corazón de la pulga :c",
+];
+
+export default phrases;
